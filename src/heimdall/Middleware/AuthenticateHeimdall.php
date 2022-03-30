21236649c64bc4e9931d8fd43120855b0a2e927f
guipalermo <guipalermo@gmail.com>
2022-03-30 17:27:03 -0300

    create auth middleware

---
+++ src/heimdall/Middleware/AuthenticateHeimdall.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Heimdall\Middleware;
+
+use Closure;
+use Exception;
+use Heimdall\Service\HeimdallService;
+use Illuminate\Http\Request;
+
+class AuthenticateHeimdall
+{
+    /**
+     * Get the path the user should be redirected to when they are not authenticated.
+     *
+     * @param  Request  $request
+     * @return string|null
+     */
+    protected function redirectTo($request): ?string
+    {
+        return (!$request->expectsJson()) ? '/error' : null;
+    }
+
+    public function handle($request, Closure $next, string $role = null)
+    {
+        try {
+            $token = request()->bearerToken();
+
+            if(isset($token)) {
+
+                $heimdallService = new HeimdallService(env("HEIMDALL_PROJECT"));
+                $heimdallService->setAccessToken($token);
+
+                if(isset($role) && !$heimdallService->accessTokenHasRole($role)) {
+                    throw new Exception('Permission denied', 422);
+                }
+            }
+        } catch (Exception $exception) {
+            return response()->json([
+                'code' => $exception->getCode(),
+                'message' => $exception->getMessage(),
+                'status' => 'error'
+            ]);
+        }
+
+        return $next($request);
+    }
+}
